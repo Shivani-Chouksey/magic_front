de74854f4ffaf79c94accceb017a180ed101c407
Shivani-Chouksey <122255940+Shivani-Chouksey@users.noreply.github.com>
2024-09-22 22:25:14 +0530

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,13 @@
 import "@/styles/globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 export const BaseUrl="http://localhost:8000";
 
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return<>
+   <Component {...pageProps} />
+   <ToastContainer />
+  </>;
 }
